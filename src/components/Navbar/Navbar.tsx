2e7b916d1415bd8d485e3053c56bc3a0d1808ec6
Sh0bhit <sguptamds94@gmail.com>
2023-09-11 15:54:58 +0530

    Added instagram link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,18 @@
-import Discord from "../icons/Discord";
+import Instagram from "../icons/Instagram";
 
 export default function Navbar() {
   return (
     <div className="flex gap-3 w-full justify-between font-poppins px-5 py-5 text-whiteText text-[20px]">
       <h1>NSE Pirates</h1>
       <div
-        className="flex cursor-pointer items-center gap-5"
+        className="flex cursor-pointer items-center gap-2"
         onClick={() => {
-          window.open("https://discord.gg/MRFTdQ9bUb", "_blank");
+          window.open("https://www.instagram.com/hazard.trades/", "_blank");
         }}
       >
-        <div className="">Join Discord</div>
-        <Discord width={25} height={25} />
+        <div className="">Follow on Instagram</div>
+
+        <Instagram width={25} height={25} />
       </div>
     </div>
   );
