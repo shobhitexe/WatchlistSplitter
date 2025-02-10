48141586ab704211fc791795fa5c19b65a838d33
Shobhit Gupta <sguptamds94@gmail.com>
2025-02-10 15:06:26 +0530

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import DiscordIcon from "../icons/DiscordIcon";
 export default function Navbar() {
   return (
     <div className="flex gap-3 w-full justify-between font-poppins px-5 py-5 text-whiteText text-[20px]">
-      <h1>NSE Pirates</h1>
+      <h1>Watchlist Splitter</h1>
       <Link
         className="flex cursor-pointer items-center gap-2"
         href={"https://discord.gg/MRFTdQ9bUb"}
